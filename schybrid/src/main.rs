a88c8ce2a6a10ea30c8f5bacb4846e1d08ca26ef
Shreepad S <shreepads@gmail.com>
2022-07-02 19:01:40 +0530

    Init schedule to_csv_file method

--- schybrid/src/main.rs
+++ schybrid/src/main.rs
@@ -38,6 +38,9 @@ fn find_optimal_seating(seats: u64, filepath: String) -> Result<u8, Box<dyn Erro
         // Get the schedule for the best combination
         let best_sched = TeamsSchedule::new(teams, best_combination, seats);
 
+        // Write to CSV
+        best_sched.to_csv_file("./schedule.csv".to_string());
+
     } else {
         println!("There is no valid combination");
     }
--- teams_sched/src/lib.rs
+++ teams_sched/src/lib.rs
@@ -1,6 +1,11 @@
 // Copyright (c) 2022 Shreepad Shukla
 // SPDX-License-Identifier: AGPL-3.0-only
 
+use std::error::Error;
+use std::fs::File;
+
+use csv::Writer;
+
 use teams_info::Teams;
 use teams_info::Combination;
 
@@ -21,6 +26,15 @@ impl TeamsSchedule {
         }
     }
 
+    pub fn to_csv_file(&self, file_path: String) -> Result<(), Box<dyn Error>> {
+
+        let mut wtr = Writer::from_path(file_path)?;
+
+        wtr.write_record(&["Teamid", "Teamsize", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"])?;
+
+        wtr.flush()?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
